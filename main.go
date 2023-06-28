bc32e73c04df82dcd1b5eee8109dc38e114dd33e
Philipe <philipep.galdino@gmail.com>
2023-06-28 00:16:35 -0300

    feat: added endpoint to generate public & private keys

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"encoding/hex"
 	"fmt"
+	"log"
 	"net/http"
 )
 
@@ -9,8 +14,28 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Request sent to: %s\n", r.URL.Path)
 }
 
+func generateKeys(w http.ResponseWriter, r *http.Request) {
+	privateKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+
+	if err != nil {
+		http.Error(w, "Error while generating keys", http.StatusInternalServerError)
+		return
+	}
+
+	publicKey := privateKey.PublicKey
+
+	privateKeyBytes := privateKey.D.Bytes()
+	publicKeyBytes := elliptic.Marshal(elliptic.P256(), publicKey.X, publicKey.Y)
+
+	fmt.Fprintf(w, "Private key: %s\n", hex.EncodeToString(privateKeyBytes))
+	fmt.Fprintf(w, "Public key: %s\n", hex.EncodeToString(publicKeyBytes))
+
+}
+
 func main() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/keys", generateKeys)
+
 	fmt.Println("Server listening on port 8080")
-	http.ListenAndServe(":8080", nil)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
