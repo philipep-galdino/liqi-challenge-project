1e28f082de046bf559f4f7d4173e32d33eab6a35
Philipe <philipep.galdino@gmail.com>
2023-06-28 00:15:43 -0300

    feat: added tests for generating keys endpoint

--- main_test.go
+++ main_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
@@ -25,5 +26,29 @@ func TestHandler(t *testing.T) {
 
 	if rr.Body.String() != expected {
 		t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
+	}
+}
+
+func TestGenerateKeysHandler(t *testing.T) {
+	req, err := http.NewRequest("GET", "/keys", nil)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(generateKeys)
+
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: I don't want %v, I want %v",
+			status, http.StatusOK)
+	}
+
+	responseBody := rr.Body.String()
+
+	if !strings.Contains(responseBody, "Private key:") || !strings.Contains(responseBody, "Public key:") {
+		t.Errorf("handler returned unexpected body: %v", responseBody)
 	}
 }
