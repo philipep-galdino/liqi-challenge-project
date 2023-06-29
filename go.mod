5bb438de20316b3436d27be08968ae23fdb442d6
Philipe <philipep.galdino@gmail.com>
2023-06-29 13:48:14 -0300

    feat: setup sqs send message function

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.20
 require golang.org/x/crypto v0.10.0
 
 require (
+	github.com/aws/aws-lambda-go v1.41.0
 	github.com/aws/aws-sdk-go v1.44.292 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	golang.org/x/sys v0.9.0 // indirect
--- pkg/sqs/sqs.go
+++ pkg/sqs/sqs.go
@@ -1,6 +1,28 @@
 package sqs
 
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sqs"
+)
+
 type Transaction struct {
 	To    string `json:"to"`
 	Value string `json: "value"`
+}
+
+func SendMessage(queueURL string, messageBody string) (*sqs.SendMessageOutput, error) {
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	})
+
+	svc := sqs.New(sess)
+
+	result, err := svc.SendMessage(&sqs.SendMessageInput{
+		DelaySeconds: aws.Int64(10),
+		MessageBody: aws.String(messageBody),
+		QueueUrl: aws.String(queueURL)
+	})
+
+	return result, err
 }
